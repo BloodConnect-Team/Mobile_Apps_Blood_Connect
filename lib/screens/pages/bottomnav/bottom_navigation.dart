78b5b454dea0da281370ab74bfc425a221d73205
sukmarizki04 <sukmarizki04@gmail.com>
2023-06-14 02:16:02 +0700

    besoklagi

--- lib/screens/pages/bottomnav/bottom_navigation.dart
+++ lib/screens/pages/bottomnav/bottom_navigation.dart
@@ -1,9 +1,9 @@
-import 'package:blood_connect/color/color.dart';
 import 'package:blood_connect/screens/pages/home_page_screen.dart';
 import 'package:blood_connect/screens/pages/menu/blood_screen.dart';
 import 'package:blood_connect/screens/pages/menu/profile_screen.dart';
 import 'package:blood_connect/screens/pages/menu/search_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 class BottomNavigation extends StatefulWidget {
   const BottomNavigation({super.key});
@@ -32,34 +32,33 @@ class _BottomNavigationState extends State<BottomNavigation> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: _listPage[_selectedBottom],
-      bottomNavigationBar: BottomNavigationBar(
-        unselectedIconTheme: const IconThemeData(
-          color: Colors.blueGrey,
-        ),
-        unselectedItemColor: Colors.grey,
-        iconSize: 30,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.home,
-            ),
-            label: "Home",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.search,
-            ),
-            label: "Search",
-          ),
-          BottomNavigationBarItem(icon: Icon(Icons.bloodtype), label: "Blood"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.person_outline_outlined), label: "Profil")
-        ],
-        selectedItemColor: primaryColor,
-        onTap: onItemTap,
-        type: BottomNavigationBarType.fixed,
-        currentIndex: _selectedBottom,
-        showUnselectedLabels: false,
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        child: GNav(
+            gap: 5,
+            tabBorderRadius: 30,
+            haptic: true,
+            color: Colors.redAccent,
+            activeColor: Colors.redAccent,
+            backgroundColor: Colors.white,
+            tabBackgroundColor: Colors.grey,
+            iconSize: 25,
+            padding: const EdgeInsets.all(10),
+            onTabChange: (value) {
+              onItemTap(value);
+            },
+            tabs: const [
+              GButton(
+                icon: Icons.home,
+                text: 'Home',
+              ),
+              GButton(icon: Icons.search, text: 'Search'),
+              GButton(icon: Icons.bloodtype_sharp, text: 'Blood'),
+              GButton(
+                icon: Icons.person,
+                text: 'Profil',
+              )
+            ]),
       ),
     );
   }
--- lib/screens/pages/mainpage/mobile_unit_screen.dart
+++ lib/screens/pages/mainpage/mobile_unit_screen.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class MobileUnitScreen extends StatelessWidget {
+class MobileUnitScreen extends StatefulWidget {
   const MobileUnitScreen({super.key});
 
+  @override
+  State<MobileUnitScreen> createState() => _MobileUnitScreenState();
+}
+
+class _MobileUnitScreenState extends State<MobileUnitScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,6 +19,137 @@ class MobileUnitScreen extends StatelessWidget {
           style: TextStyle(fontSize: 17, color: Colors.black),
         ),
         backgroundColor: Colors.white,
+      ),
+      body: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 20, horizontal: 10.0),
+            child: SizedBox(
+              height: 50,
+              child: TextField(
+                decoration: InputDecoration(
+                    hintText: 'Search .......',
+                    suffixIcon: IconButton(
+                        onPressed: () {}, icon: const Icon(Icons.search)),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0))),
+              ),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(top: 2, right: 220),
+            child: Text(
+              'Jadwal Mobil Unit',
+              style: TextStyle(fontWeight: FontWeight.w500, fontSize: 13),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Container(
+              width: double.infinity,
+              decoration: const BoxDecoration(boxShadow: [
+                BoxShadow(
+                  color: Color(0xffDDDDDD),
+                  blurRadius: 6.0,
+                  spreadRadius: 2.0,
+                  offset: Offset(0.0, 0.0),
+                )
+              ], borderRadius: BorderRadius.all(Radius.circular(10))),
+              child: Row(
+                children: [
+                  SizedBox(child: Image.asset('assets/img/icon3.png')),
+                  const Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(bottom: 10.0, right: 5.0),
+                        child: Text(
+                          'Herfort Brirtis Hospital',
+                          style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.black),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 11.0, bottom: 5.0),
+                        child: Text(
+                          'UTD PMI Kota Lhokseumawe',
+                          style: TextStyle(fontSize: 13, color: Colors.black26),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(right: 45.0, bottom: 5.0),
+                        child: Text('Target Donor: 50',
+                            style:
+                                TextStyle(fontSize: 15, color: Colors.black26)),
+                      ),
+                      Text('JL.Perkutut GG Gereja II',
+                          style:
+                              TextStyle(fontSize: 15, color: Colors.black26)),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Container(
+              width: double.infinity,
+              decoration: const BoxDecoration(boxShadow: [
+                BoxShadow(
+                  color: Color(0xffDDDDDD),
+                  blurRadius: 6.0,
+                  spreadRadius: 2.0,
+                  offset: Offset(0.0, 0.0),
+                )
+              ], borderRadius: BorderRadius.all(Radius.circular(10))),
+              child: Row(
+                children: [
+                  SizedBox(child: Image.asset('assets/img/icon3.png')),
+                  const Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(bottom: 10.0, right: 5.0),
+                        child: Text(
+                          'Herfort Brirtis Hospital',
+                          style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.black),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 11.0, bottom: 5.0),
+                        child: Text(
+                          'UTD PMI Kota Lhokseumawe',
+                          style: TextStyle(fontSize: 13, color: Colors.black26),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(right: 45.0, bottom: 5.0),
+                        child: Text('Target Donor: 50',
+                            style:
+                                TextStyle(fontSize: 15, color: Colors.black26)),
+                      ),
+                      Text('JL.Perkutut GG Gereja II',
+                          style:
+                              TextStyle(fontSize: 15, color: Colors.black26)),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
